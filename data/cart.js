e5d90fe39cae9e3c0d540f5acc5f5a149f3c547c
Daniela Tanase <tanase.ana.daniela@gmail.com>
2024-09-30 14:30:14 +0300

    Generate HTML for the order summary

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,18 @@
-export const cart = [];
+export const cart = [
+	{
+		productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    //image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+    //name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    //priceCents: 1090,
+		quantity: 2,
+	}, {
+		productId: '15b6fc6f-327a-4ec4-896f-48634985a3d',
+		/*image: "images/products/intermediate-composite-basketball.jpg",
+		name: "Intermediate Size Basketball",
+		priceCents: 2095,*/
+		quantity: 1
+	}
+];
 
 export function addToCart(productId) {
 	let matchingItem;
