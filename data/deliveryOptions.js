2986108221981e597238d1eaff332c42deefd7e4
Daniela Tanase <tanase.ana.daniela@gmail.com>
2024-10-08 15:01:01 +0300

    Make delivery  skip the weekend

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -12,10 +12,22 @@ export function getDeliveryOption(deliveryOptionId) {
 	return deliveryOption || deliveryOption[0];
 }
 
+function isWeekend(date) {
+	const myDate = date.format('dddd');
+	return myDate === 'Saturday' || myDate === 'Sunday';
+}
 
 export function calculateDeliveryDate(deliveryOption) {
-	const today = dayjs();
-  const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
+	let deliveryDate = dayjs();
+	let remainingDays = deliveryOption.deliveryDays;
+
+	while (remainingDays > 0) {
+		deliveryDate = deliveryDate.add(1, 'day');
+
+		if (!isWeekend(deliveryDate)) {
+			remainingDays--;
+		}
+	}
 	const dateString = deliveryDate.format('dddd, MMMM D');
 	return dateString;
 }
