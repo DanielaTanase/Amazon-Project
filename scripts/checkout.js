e29cfd8ec80190e01c66a6211f1ee310fadacf18
Daniela Tanase <tanase.ana.daniela@gmail.com>
2024-10-15 15:22:03 +0300

    Load products from backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,14 +2,19 @@ import { renderCheckoutHeader } from "../styles/pages/checkout/checkoutHeader.js
 import { renderOrderSummary, } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 //import '../data/cart-class.js';
-import '../data/backend-practice.js';
+//import '../data/backend-practice.js';
+
+import { loadProducts } from "../data/products.js";
+
+loadProducts(() => {
+	renderCheckoutHeader();
+
+	renderOrderSummary();
+
+	renderPaymentSummary();
+});
 
 
-renderCheckoutHeader();
-
-renderOrderSummary();
-
-renderPaymentSummary();
 
 
 
